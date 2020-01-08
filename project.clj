175ddabe0437034c63f475eecb08cf80250f2d6c
Magnar Sveen <magnars@gmail.com>
2020-01-08 12:40:11 +0100

    Add lein kaocha alias

--- project.clj
+++ project.clj
@@ -6,4 +6,5 @@
   :dependencies [[ch.digitalfondue.jfiveparse/jfiveparse "0.5.3"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.1"]
                                   [lambdaisland/kaocha "0.0-529"]
-                                  [kaocha-noyoda "2019-06-03"]]}})
+                                  [kaocha-noyoda "2019-06-03"]]
+                   :aliases {"kaocha" ["run" "-m" "kaocha.runner"]}}})
