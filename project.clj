cbd328915ef26e8f6740f0d09729ff87329775db
Christian Johansen <christian@cjohansen.no>
2022-03-07 10:32:25 +0100

    Prepare new release with Maven friendly version "number"

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject html5-walker "2020-01-08"
+(defproject html5-walker "2022.03.07"
   :description "Search and replace html5."
   :url "https://github.com/magnars/html5-walker"
   :license {:name "Eclipse Public License"
